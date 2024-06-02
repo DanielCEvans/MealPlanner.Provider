c2a8e56cb944519bb638a5272cd6d0308ac58cf2
Daniel Evans <daniel.evans@dyedurham.com>
2024-06-02 19:12:08 +1000

    feat: initial migration

--- src/MealPlanner.Provider.Endpoint/Program.cs
+++ src/MealPlanner.Provider.Endpoint/Program.cs
@@ -1,9 +1,16 @@
+using MealPlanner.Provider.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MealPlannerContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
--- src/MealPlanner.Provider.Persistence/MealPlannerContext.cs
+++ src/MealPlanner.Provider.Persistence/MealPlannerContext.cs
@@ -1,6 +1,17 @@
+using MealPlanner.Provider.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace MealPlanner.Provider.Persistence;
 
-public class MealPlannerContext
+public class MealPlannerContext : DbContext
 {
+    public MealPlannerContext(DbContextOptions<MealPlannerContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Meal> Meals { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<MealIngredients> MealIngredients { get; set; }
     
 }
+
