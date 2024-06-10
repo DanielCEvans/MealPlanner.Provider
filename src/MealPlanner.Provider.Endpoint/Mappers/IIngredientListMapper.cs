3abe6770270da472636a30a99118ae6918b01089
Daniel Evans <daniel.evans@dyedurham.com>
2024-06-10 20:18:16 +1000

    fix: remove mappers

--- src/MealPlanner.Provider.Endpoint/Mappers/IIngredientListMapper.cs
+++
@@ -1,8 +0,0 @@
-using MealPlanner.Provider.Persistence.Models;
-
-namespace MealPlanner.Provider.Endpoint.Mappers;
-
-public interface IIngredientListMapper
-{
-    // IngredientsList MealIngredientsToIngredientsList(List<MealIngredients> mealIngredients);
-}
