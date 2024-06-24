430f81f5d0035c1079e4ff45378ccfa2c94bc1e3
Daniel Evans <daniel.evans@dyedurham.com>
2024-06-24 17:01:42 +1000

    feat: add CORS policy

--- src/MealPlanner.Provider.Endpoint/Program.cs
+++ src/MealPlanner.Provider.Endpoint/Program.cs
@@ -5,10 +5,23 @@ using MealPlanner.Provider.Persistence;
 using MealPlanner.Provider.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -26,6 +39,8 @@ builder.Services.AddDbContext<MealPlannerContext>(options =>
 );
 
 var app = builder.Build();
+
+app.UseCors(myAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
